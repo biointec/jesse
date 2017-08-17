6e2200687b257599162f017aab8850c74ecac5d4
thpar <thpar@psb.ugent.be>
2017-08-17 16:31:17 +0200

    Longer running methods get access to a TaskMonitor and a cancelled check

--- src/codegenerating/TaskMonitor.java
+++
@@ -1,7 +0,0 @@
-package codegenerating;
-
-public interface TaskMonitor {
-	public void setProgress(double progress);
-	public void setStatusMessage(String msg);
-	public void setTitle(String title);
-}
---
+++ src/progress/TaskMonitor.java
@@ -0,0 +1,40 @@
+package progress;
+
+/**
+ * Lightweight task monitor interface to allow methods to report their 
+ * progress to a client. 
+ * 
+ * @author Thomas Van Parys
+ *
+ */
+public interface TaskMonitor {
+	
+	/**
+	 * 
+	 * @param title general description about the running task
+	 */
+	public void setTitle(String title);
+	
+	/**
+	 * 
+	 * @param progress the progress of your task on a scale from 0 to 1
+	 */
+	public void setProgress(double progress);
+	
+	/**
+	 * 
+	 * @param msg information about which part of the task is currently in progress 
+	 */
+	public void setStatusMessage(String msg);
+	
+	/**
+	 * Signal the ongoing task it should stop as soon as it can, clean up and return
+	 */
+	public void cancel();
+	
+	/**
+	 * 
+	 * @return true if this task got cancelled
+	 */
+	public boolean isCancelled();
+}
